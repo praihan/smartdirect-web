345dbb8a9e2ac04b02f6cbffdca700f18db34196
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-12-22 16:41:16 -0500

    Fix ember-simple-auth version wonkyness

--- app/mixins/application-route-mixin.js
+++ app/mixins/application-route-mixin.js
@@ -1,22 +1,6 @@
 import Ember from 'ember';
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+// Note that this is from the auth0 package, not simple-auth
+import ApplicationRouteMixin from 'ember-simple-auth-auth0/mixins/application-route-mixin';
 
 export default Ember.Mixin.create(ApplicationRouteMixin, {
-  routeAfterAuthentication: 'index',
-
-  sessionAuthenticated() {
-    const attemptedTransition = this.get('session.attemptedTransition');
-    const cookies = Ember.getOwner(this).lookup('service:cookies');
-    const redirectTarget = cookies.read('ember_simple_auth-redirectTarget');
-
-    if (attemptedTransition) {
-      attemptedTransition.retry();
-      this.set('session.attemptedTransition', null);
-    } else if (redirectTarget) {
-      this.transitionTo(redirectTarget);
-      cookies.clear('ember_simple_auth-redirectTarget');
-    } else {
-      this.transitionTo(this.get('routeAfterAuthentication'));
-    }
-  }
 });
