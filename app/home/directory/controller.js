e3d21e7c6cb96b31ed6f8aa97c00f205024d3d3f
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-12-26 18:41:22 -0500

    Deselect directory after deleting

--- app/home/directory/controller.js
+++ app/home/directory/controller.js
@@ -14,31 +14,49 @@ const {
 } = Ember;
 
 export default Controller.extend({
-  selectedDirectory: null,
+  /**
+   * The selected directory or file.
+   */
+  selectedDirOrFile: null,
+  /**
+   * A binding to the text input for the name of a new file/directory.
+   */
   newDirOrFileNameText: '',
 
-  loadingDirectories: computed('model.children.@each.isNew', function() {
+  /**
+   * An array of child directories that have not been saved to the backend
+   * by our adapter yet.
+   */
+  pendingDirectories: computed('model.children.@each.isNew', function() {
     return this.get('model.children').filterBy('isNew');
   }),
-  isLoading: computed.notEmpty('loadingDirectories'),
+  /**
+   * Loading state is if we have any pending directories or files that need
+   * updating on the backend.
+   */
+  isLoading: computed.notEmpty('pendingDirectories'),
 
+  /**
+   * If we change directories, drop our selected item.
+   */
   _unselectOnModelChange: observer('model', function() {
-    this.set('selectedDirectory', null);
+    this.set('selectedDirOrFile', null);
   }),
 
   actions: {
-    setSelectedDirectory(directory) {
-      this.set('selectedDirectory', directory);
+    setSelectedDirOrFile(dirOrFile) {
+      this.set('selectedDirOrFile', dirOrFile);
       // We need to highlight the selected directory
       //   1. Unhighlight all directories
       //   2. Highlight only the selected directory(ies)
       const rows = $('table tr.table-entry').not('.loading');
       rows.removeClass('active');
-      if (directory == null) {
+      if (dirOrFile == null) {
         return;
       }
+      // TODO: Account for files too
       rows.each((i, tr) => {
-        if ($.attr(tr, 'data-id') === directory.get('id')) {
+        if ($.attr(tr, 'data-id') === dirOrFile.get('id')) {
           $(tr).addClass('active');
         }
       });
@@ -59,6 +77,15 @@ export default Controller.extend({
     destroyDirOrFile(dirOrFile) {
       assert('directory or file provided', dirOrFile);
       dirOrFile.destroyRecord()
+        .then(() => {
+          // if we destroy our selected one, it can't be
+          // selected any more.
+          // Note that this will happen only after the backend
+          // verifies that it's gone.
+          if (this.get('selectedDirOrFile') == dirOrFile) {
+            this.set('selectedDirOrFile', null);
+          }
+        })
         .catch(() => {
           dirOrFile.rollback();
         });
