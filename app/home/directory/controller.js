924e68d318d4c12d531f377e02a1de2e68769754
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-12-26 04:46:14 -0500

    Add selectable directory

--- app/home/directory/controller.js
+++ app/home/directory/controller.js
@@ -2,7 +2,28 @@ import Ember from 'ember';
 
 const {
   Controller,
+  $,
 } = Ember;
 
 export default Controller.extend({
+  selectedDirectory: null,
+
+  actions: {
+    setSelectedDirectory(directory) {
+      this.set('selectedDirectory', directory);
+      // We need to highlight the selected directory
+      //   1. Unhighlight all directories
+      //   2. Highlight only the selected directory(ies)
+      const rows = $('table tr.table-entry');
+      rows.removeClass('active');
+      if (directory == null) {
+        return;
+      }
+      rows.each((i, tr) => {
+        if ($.attr(tr, 'data-id') === directory.get('id')) {
+          $(tr).addClass('active');
+        }
+      });
+    }
+  }
 });
