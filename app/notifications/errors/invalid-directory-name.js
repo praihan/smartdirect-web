9b274ae7fd0f3307a66f8568306fc13f732efc4f
Pranjal Raihan <pranjal@pranjal-raihan.com>
2017-02-13 23:55:25 -0500

    Add custom notify styling.

--- app/notifications/errors/invalid-directory-name.js
+++ app/notifications/errors/invalid-directory-name.js
@@ -1,3 +1,5 @@
 export default function invalidDirectoryName(applicationInstance, notify/*, apiError */) {
-  notify.alert('Please enter a valid directory name.');
+  notify.alert('Please enter a valid directory name.', {
+    closeAfter: 5000,
+  });
 }
