70f679d35657e2b5837f11fc040939c3d16f9842
Pranjal Raihan <pranjal@pranjal-raihan.com>
2017-02-17 01:29:54 -0500

    Add comment about injections in helpers.

--- app/helpers/format-directory-name.js
+++ app/helpers/format-directory-name.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
+// TODO: Use i18n when possible.
+// see https://github.com/emberjs/ember.js/issues/11021#issuecomment-161987298
+
 export function formatDirectoryName([name]) {
   Ember.assert('name must be a string', typeof name === 'string');
   if (name == '') {
